c95fd755dab7883c96b76c924017be1ca91039e6
jermyLin <495550623@qq.com>
2017-08-07 19:38:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import mHeader from 'components/m-header/m-header'
+  import tab from 'components/tab/tab'
+  export default {
+    components: {
+      mHeader,
+      tab
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fff;
+  }
+</style>
